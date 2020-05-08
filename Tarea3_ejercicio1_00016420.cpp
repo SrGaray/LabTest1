f0a653ca0e7eeedab51f323af3b2793e03b874df
SrGaray <00016420@uca.edu.sv>
2020-05-08 11:16:09 -0600

    Laboratorio3

---
+++ Tarea3_ejercicio1_00016420.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1,num2,num3,prom;
+    
+    cout<<"Ingresar numeros: "<<endl;
+    cin>>num1>>num2>>num3;
+
+    prom = int (num1+num2+num3) / 3;
+    cout << "Su promedio es: " <<prom <<endl;
+
+    return 0;
+}
+  
+
---
+++ Tarea3_ejercicio2_00016420.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float Pi = 3.1416;
+    float rad;
+    
+    cout<<"Su radio es? "<<endl;
+    cin>>rad;
+    cout<<"Su area es: "<<endl << (rad*rad)*Pi<<endl;
+    cout<<"Su perimetro es: "<<endl << 2*Pi*rad<<endl;
+
+    return 0;
+}
+    
+
+   
+
+
